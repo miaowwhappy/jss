53f9ea80d8e8e9626b266f14c6db30878d776b9a
miaowwhappy <183306832@qq.com>
2017-10-07 12:49:20 +0800

    Initial commit

---
+++ jss.js
@@ -0,0 +1,31 @@
+;(function($){
+	var dialog2 = function(outConfig){
+		var me = this;
+		this.config = {
+			width : 100,
+		};
+		$.extend(this.config,outConfig);
+		console.log(this.config);
+
+		this.mask = $("<div class='mask'></div>");
+		this.body = $("body");
+		this.creat();
+		this.mask.click(function(){
+			me.close();
+		})
+	};
+
+	dialog2.prototype = {
+		creat:function(){
+			var me = this,
+				mask = this.mask,
+				body = this.body;
+			body.append(mask);
+		},
+		close:function(){
+			this.mask.remove();
+		}
+
+	}
+	window.dialog2 = dialog2;
+})(jQuery);
